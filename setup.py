bb125bfa2dc91817976c6edff69ea572c159a108
xiaomizhoubaobei <qixiaoxin@stu.sqxy.edu.cn>
2025-01-04 16:28:27 +0800

    新增ERNIE-Lite-Pro-128K模型

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 setup(
     name="MZAPI",
-    version="0.2.0",
+    version="0.2.1",
     description="米粥SDK",
     license_expressions=["MIT"],
     packages=find_packages(),
