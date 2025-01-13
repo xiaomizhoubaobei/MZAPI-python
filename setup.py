ddb85630edc49aa9f51f84dff4987b0671992499
xiaomizhoubaobei <qixiaoxin@stu.sqxy.edu.cn>
2025-01-13 19:43:52 +0800

    feat(MB): 添加 BloomZ-7B 模型支持并更新相关配置
    - 新增 seventB 类以支持百度 BloomZ-7B 模型
    - 更新 GitHub Pages 部署配置，增加必要参数
    - 将 MZAPI 版本号从 0.3.4 升级到 0.3.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 setup(
     name="MZAPI",
-    version="0.3.4",
+    version="0.3.5",
     description="米粥SDK",
     license_expressions=["MIT"],
     packages=find_packages(),
